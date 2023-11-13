0d5b7f6326deb03d2a838977b8001d407a86442b
Henrik Bjørbekk <henrikbjorbekk@Henriks-MacBook-Pro-Skole.local>
2023-11-13 13:25:03 +0100

    fix

--- f1/src/components/teams.tsx
+++ f1/src/components/teams.tsx
@@ -1,24 +1,24 @@
-import React from "react";
+// import React from "react";
 
-const Teams = () => {
-  // Placeholder data
-  const teams = [
-    { id: 1, manufacturer: "Team 1", drivers: ["Driver 1", "Driver 2"] },
-    // ... other teams
-  ];
+// const Teams = () => {
+//   // Placeholder data
+//   const teams = [
+//     { id: 1, manufacturer: "Team 1", drivers: ["Driver 1", "Driver 2"] },
+//     // ... other teams
+//   ];
 
-  return (
-    <div>
-      <h2>Teams</h2>
-      <ul>
-        {teams.map((team) => (
-          <li key={team.id}>
-            {team.manufacturer} - Drivers: {team.drivers.join(", ")}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h2>Teams</h2>
+//       <ul>
+//         {teams.map((team) => (
+//           <li key={team.id}>
+//             {team.manufacturer} - Drivers: {team.drivers.join(", ")}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
-export default Teams;
+// export default Teams;
