78427fa7066ad3c419a70e3b6abf0c8138fcf47c
Henrik Bjørbekk <henrikbjorbekk@Henriks-MacBook-Pro-Skole.local>
2023-11-13 13:54:25 +0100

    forslag

--- f1/src/App.tsx
+++ f1/src/App.tsx
@@ -1,5 +1,37 @@
-function App() {
-  return <></>;
-}
+// function App() {
+//   return <></>;
+// }
 
-export default App;
+// export default App;
+
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+// import Home from "./components/Home";
+// import Drivers from "./components/Drivers";
+// import Teams from "./components/Teams";
+// import Races from "./components/Races";
+// // ... import other components
+
+// function App() {
+//   return (
+//     <Router>
+//       <nav>
+//         <Link to="/">Home</Link>
+//         <Link to="/drivers">Drivers</Link>
+//         <Link to="/teams">Teams</Link>
+//         <Link to="/races">Races</Link>
+//         {/* Add other links */}
+//       </nav>
+
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/drivers" element={<Drivers />} />
+//         <Route path="/teams" element={<Teams />} />
+//         <Route path="/races" element={<Races />} />
+//         {/* Define other routes */}
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
