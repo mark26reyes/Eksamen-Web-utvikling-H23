74983ee36a4c03946988665402a9a32738f2ccb9
Henrik Bjørbekk <henrikbjorbekk@Henriks-MacBook-Pro-Skole.local>
2023-11-13 13:25:33 +0100

    fix

---
+++ f1/src/components/races.tsx
@@ -0,0 +1,29 @@
+// import React from "react";
+
+// const Races = () => {
+//   // Placeholder data
+//   const races = [
+//     {
+//       id: 1,
+//       grandPrix: "Race 1",
+//       winnerName: "Winner 1",
+//       winnerTime: "1:30:00",
+//     },
+//     // ... other races
+//   ];
+
+//   return (
+//     <div>
+//       <h2>Races</h2>
+//       <ul>
+//         {races.map((race) => (
+//           <li key={race.id}>
+//             {race.grandPrix} - Winner: {race.winnerName}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Races;
