d93e4c10088be8331d6952fa8ee9cc227e49c199
Mark Daniel Reyes <mare044@student.kristiania.no>
2023-11-14 13:12:21 +0100

    bootstrap styling

--- f1/src/App.tsx
+++ f1/src/App.tsx
@@ -4,39 +4,59 @@ import DriversPage from "./pages/DriversPage";
 import TeamsPage from "./pages/TeamsPage";
 import RacesPage from "./pages/RacesPage";
 import QuizPage from "./pages/QuizPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/drivers">Drivers</Link>
-          </li>
-          <li>
-            <Link to="/teams">Teams</Link>
-          </li>
-          <li>
-            <Link to="/races">Races</Link>
-          </li>
-          <li>
-            <Link to="/quiz">Quiz</Link>
-          </li>
-        </ul>
-      </nav>
+    <>
+      <Router>
+        <header>
+          <nav className="navbar navbar-expand navbar-dark p-4">
+            <h1 className="text-light">F1</h1>
+            <div className="navbar-collapse justify-content-end">
+              <ul className="navbar-nav">
+                <li>
+                  <Link className="nav-link text-light fw-bold" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link text-light fw-bold" to="/drivers">
+                    Drivers
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link text-light fw-bold" to="/teams">
+                    Teams
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link text-light fw-bold" to="/races">
+                    Races
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link text-light fw-bold" to="/quiz">
+                    Quiz
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </header>
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/drivers" element={<DriversPage />} />
-        <Route path="/teams" element={<TeamsPage />} />
-        <Route path="/races" element={<RacesPage />} />
-        <Route path="/quiz" element={<QuizPage />} />
-        {/* Define other routes */}
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/drivers" element={<DriversPage />} />
+          <Route path="/teams" element={<TeamsPage />} />
+          <Route path="/races" element={<RacesPage />} />
+          <Route path="/quiz" element={<QuizPage />} />
+          {/* Define other routes */}
+        </Routes>
+      </Router>
+      <header></header>
+    </>
   );
 }
 
