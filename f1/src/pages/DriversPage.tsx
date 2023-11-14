bf978bfd45ba16344373d5147ab4ae6d54996ddc
Mark Daniel Reyes <mare044@student.kristiania.no>
2023-11-14 13:25:17 +0100

    drivers item og list konsept

--- f1/src/pages/DriversPage.tsx
+++ f1/src/pages/DriversPage.tsx
@@ -1,8 +1,11 @@
+import DriversItem from "../components/drivers/DriversItem";
+
 const DriversPage = () => {
   return (
     <section>
       <h1>Welcome to Driver App</h1>
       <p>This is the drivers page of the app.</p>
+      <DriversItem />
     </section>
   );
 };
