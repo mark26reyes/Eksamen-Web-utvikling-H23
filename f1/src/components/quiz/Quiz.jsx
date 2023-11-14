3ee753cba893b5016e7f9065863e67921ef4abec
Henrik Bjørbekk <henrikbjorbekk@Henriks-MacBook-Pro-Skole.local>
2023-11-14 13:23:27 +0100

    quiz

---
+++ f1/src/components/quiz/Quiz.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+const questions = [
+  {
+    questionText: "Who won the F1 World Championship in 2020?",
+    answerOptions: [
+      { answerText: "Lewis Hamilton", isCorrect: true },
+      { answerText: "Max Verstappen", isCorrect: false },
+      { answerText: "Sebastian Vettel", isCorrect: false },
+      { answerText: "Daniel Ricciardo", isCorrect: false },
+    ],
+  },
+  // Add more questions here
+];
+
+function Quiz() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerButtonClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  return (
+    <div className="quiz">
+      {showScore ? (
+        <div className="score-section">
+          You scored {score} out of {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map(
+              (answerOption, index) => (
+                <button
+                  key={index}
+                  onClick={() =>
+                    handleAnswerButtonClick(answerOption.isCorrect)
+                  }
+                >
+                  {answerOption.answerText}
+                </button>
+              )
+            )}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Quiz;
