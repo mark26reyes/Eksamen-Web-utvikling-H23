8035d42d5a17305aebf090820c0b853dac424b91
Mark Daniel Reyes <mare044@student.kristiania.no>
2023-11-15 12:09:35 +0100

    lagt til metoder i F1Service

---
+++ f1/src/services/F1Service.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const F1Service = (() => {
+  const driverController = "";
+  const teamsController = "";
+  const racesController = "";
+  const quizController = "";
+
+  const getAll = async () => {};
+
+  const putDriver = async () => {};
+
+  const postDriver = async () => {};
+
+  return {
+    getAll,
+    putDriver,
+    postDriver,
+  };
+})();
+
+export default F1Service;
