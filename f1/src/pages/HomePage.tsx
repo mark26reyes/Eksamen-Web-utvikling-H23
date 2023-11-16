0eb1f49869f7b7ceeb65dabe91049708fc34653d
Mark Daniel Reyes <mare044@student.kristiania.no>
2023-11-16 15:01:50 +0100

    la til container i home page

--- f1/src/pages/HomePage.tsx
+++ f1/src/pages/HomePage.tsx
@@ -2,13 +2,15 @@ import AddDrivers from "../components/drivers/AddDrivers";
 
 const HomePage = () => {
   return (
-    <section className="mt-5">
-      <br />
-      <br />
-      <h1>Welcome to the F1 Event</h1>
-      <br />
-      <AddDrivers />
-    </section>
+    <div className="container">
+      <section className="mt-5">
+        <br />
+        <br />
+        <h1>Welcome to the F1 Event</h1>
+        <br />
+        <AddDrivers />
+      </section>
+    </div>
   );
 };
 
