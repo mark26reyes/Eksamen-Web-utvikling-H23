365f295a591aeff2cd567f8376255dd106a04deb
dapr003 <dapr003@student.kristiania.no>
2023-11-16 13:45:04 +0100

    Fikset baseURL i Teams

--- f1/src/components/teams/TeamsItem.jsx
+++ f1/src/components/teams/TeamsItem.jsx
@@ -1,12 +1,11 @@
+import F1Service from "../../services/F1Service";
+
 const TeamsItem = ({ manufactur, image, driverName, driverName2 }) => {
+  const baseURL = F1Service.getBaseUrl();
   return (
     <section>
       <h3>{manufactur}</h3>
-      <img
-        src={`http://localhost:5008${image}`}
-        width={"400px"}
-        alt="Bilde av teams"
-      />
+      <img src={`${baseURL}/${image}`} width={"400px"} alt="Bilde av teams" />
       <p>{driverName}</p>
       <p>{driverName2}</p>
     </section>
