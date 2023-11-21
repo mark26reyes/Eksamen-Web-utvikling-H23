e22d509ef9a9ef467adb740b283956f310f5c4f5
Mark Daniel Reyes <mare044@student.kristiania.no>
2023-11-21 14:11:49 +0100

    styling i quiz

--- f1/src/pages/QuizPage.tsx
+++ f1/src/pages/QuizPage.tsx
@@ -2,12 +2,20 @@ import Quiz from "../components/quiz/Quiz";
 
 const QuizPage = () => {
   return (
-    <section className="container mt-5">
-      <br />
-      <br />
-      <h1>QuizPage</h1>
-      <Quiz />
-    </section>
+    <>
+      <img
+        src="/public/pitstop.jpg"
+        width={"100%"}
+        alt="Bilde av redbull bil"
+        className="img-fluid mx-auto d-block"
+      />
+      <section className="container">
+        <br />
+        <br />
+        <h1 className="border-bottom border-dark pb-4">QuizPage</h1>
+        <Quiz />
+      </section>
+    </>
   );
 };
 
