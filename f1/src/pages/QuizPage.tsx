f99286b023207da557f182bc4efcbef8ff504138
Mark Daniel Reyes <mare044@student.kristiania.no>
2023-11-24 13:18:52 +0100

    lagt bildet nederst i quiz page

--- f1/src/pages/QuizPage.tsx
+++ f1/src/pages/QuizPage.tsx
@@ -3,20 +3,21 @@ import Quiz from "../components/quiz/Quiz";
 const QuizPage = () => {
   return (
     <>
-      <img
-        src="/public/pitstop.jpg"
-        width={"100%"}
-        alt="Bilde av redbull bil"
-        className="img-fluid mx-auto d-block"
-      />
       <section className="container">
         <br />
         <br />
-        <h3 className="border-bottom border-dark pb-4 f1-black-font fs-1">
+        <h3 className="border-bottom border-dark pt-5 pb-5 f1-black-font fs-1">
           F1 Quiz
         </h3>
         <Quiz />
       </section>
+      <br />
+      <img
+        src="/public/pitstop.jpg"
+        width={"100%"}
+        alt="Bilde av redbull bil"
+        className="img-fluid mx-auto d-block pt-5"
+      />
     </>
   );
 };
