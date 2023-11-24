f8a162cce0e754a603cee3bab0c9e55e182f22df
MattDamian <112401935+MattDamian@users.noreply.github.com>
2023-11-24 02:41:25 +0100

    Update DriverContext.tsx

--- f1/src/contexts/DriverContext.tsx
+++ f1/src/contexts/DriverContext.tsx
@@ -1,52 +1,70 @@
-import React, { createContext, useState, useEffect, ReactNode } from "react";
+import React, {
+  createContext,
+  useState,
+  useEffect,
+  FC,
+  ReactNode,
+} from "react";
 import { IDriver } from "../interfaces/IDriver";
 import DriverService from "../services/DriverService";
 
-interface DriverContextType {
-  drivers: IDriver[];
-  searchDriverByName: (name: string) => void;
-  searchTerm: string;
+// Definerer interfacet for konteksten, som beskriver hva konteksten vil inneholde
+interface IDriverContext {
+  drivers: IDriver[]; // Liste over drivere
+  addDriver: (newDriver: IDriver) => void; // Funksjon for å legge til en ny driver
+  searchDriverByName: (name: string) => void; // Funksjon for å søke etter drivere basert på navn
+  searchTerm: string; // Søkeordet som brukes for filtrering
 }
 
-const DriverContext = createContext<DriverContextType | undefined>(undefined);
+// Oppretter konteksten med en standardverdi av null
+export const DriverContext = createContext<IDriverContext | null>(null);
 
+// Definerer typen for props som Provider-komponenten vil akseptere
 interface DriverProviderProps {
-  children: ReactNode;
+  children: ReactNode; // Barn-komponentene som vil bli innhyllet av Provider
 }
 
-export const DriverProvider: React.FC<DriverProviderProps> = ({ children }) => {
-  const [drivers, setDrivers] = useState<IDriver[]>([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [filteredDrivers, setFilteredDrivers] = useState<IDriver[]>([]);
+// Provider-komponenten som gjør konteksten tilgjengelig for sine barn
+export const DriverProvider: FC<DriverProviderProps> = ({ children }) => {
+  const [drivers, setDrivers] = useState<IDriver[]>([]); // Tilstand for å holde på listen over drivere
+  const [searchTerm, setSearchTerm] = useState(""); // Tilstand for å holde på søkeordet
 
+  // useEffect for å hente drivere når komponenten monteres
   useEffect(() => {
     getDriversFromService();
   }, []);
 
-  useEffect(() => {
-    searchDriverByName(searchTerm);
-  }, [drivers, searchTerm]);
-
+  // Funksjon for å hente drivere fra en ekstern tjeneste
   const getDriversFromService = async () => {
     const driversFromService = await DriverService.getAllDrivers();
-    setDrivers(driversFromService!);
-  };
-
-  const searchDriverByName = (name: string) => {
-    setSearchTerm(name);
-    if (name === "") {
-      setFilteredDrivers(drivers);
+    if (driversFromService) {
+      setDrivers(driversFromService);
     } else {
-      const filtered = drivers.filter((driver) =>
-        driver.name.toLowerCase().includes(name.toLowerCase())
-      );
-      setFilteredDrivers(filtered);
+      setDrivers([]);
     }
   };
 
+  // Funksjon for å legge til en ny driver i listen
+  const addDriver = (newDriver: IDriver) => {
+    setDrivers([...drivers, newDriver]);
+  };
+
+  // Funksjon for å filtrere drivere basert på søkeord
+  const searchDriverByName = (name: string) => {
+    setSearchTerm(name);
+    if (name === "") {
+      return drivers;
+    } else {
+      return drivers.filter((driver) =>
+        driver.name.toLowerCase().includes(name.toLowerCase())
+      );
+    }
+  };
+
+  // Returnerer Provider-komponenten med kontekstverdier
   return (
     <DriverContext.Provider
-      value={{ drivers: filteredDrivers, searchDriverByName, searchTerm }}
+      value={{ drivers, addDriver, searchDriverByName, searchTerm }}
     >
       {children}
     </DriverContext.Provider>
