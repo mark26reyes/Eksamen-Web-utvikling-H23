23ea4ca8322eb92798cd8be2260c397d6d7ab6c7
MattDamian <112401935+MattDamian@users.noreply.github.com>
2023-11-24 02:41:42 +0100

    Update QuizContext.tsx

--- f1/src/contexts/QuizContext.tsx
+++ f1/src/contexts/QuizContext.tsx
@@ -1,37 +1,34 @@
-import React, { createContext, useState, useContext, ReactNode } from "react";
+import React, { createContext, useState, useContext } from "react";
+import { IQuiz } from "../interfaces/IQuiz";
 
-// Assuming you have a type for Quiz, import or define it here
-// import { Quiz } from './path-to-quiz-type';
+const QuizContext = createContext<IQuiz | undefined>(undefined);
 
-interface QuizContextType {
-  quiz: any[]; // Replace 'any' with your Quiz type if available
-  setQuiz: React.Dispatch<React.SetStateAction<any[]>>; // Use the same type as for quiz
-}
-
-const QuizContext = createContext<QuizContextType | undefined>(undefined);
-
-interface QuizProviderProps {
-  children: ReactNode;
-}
-
-export const useRaces = () => {
+export const useQuiz = () => {
   const context = useContext(QuizContext);
   if (!context) {
-    throw new Error('useRaces must be used within a QuizProvider');
+    throw new Error("useQuiz must be used within a QuizProvider");
   }
   return context;
 };
 
-export const QuizProvider: React.FC<QuizProviderProps> = ({ children }) => {
-  const [quiz, setQuiz] = useState<any[]>([]); // Replace 'any' with your Quiz type if available
-
-  // Add functions to modify races here
-  const getQuizFromService = async () => {
-    // Implementation
-  };
+export const QuizProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [isQuizFinished, setIsQuizFinished] = useState(false);
 
   return (
-    <QuizContext.Provider value={{ quiz, setQuiz }}>
+    <QuizContext.Provider
+      value={{
+        currentQuestionIndex,
+        setCurrentQuestionIndex,
+        score,
+        setScore,
+        isQuizFinished,
+        setIsQuizFinished,
+      }}
+    >
       {children}
     </QuizContext.Provider>
   );
