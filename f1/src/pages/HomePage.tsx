10478f5bebee03cd5cf0eca017c14f8024fa4821
MattDamian <112401935+MattDamian@users.noreply.github.com>
2023-11-24 02:42:01 +0100

    Update HomePage.tsx

--- f1/src/pages/HomePage.tsx
+++ f1/src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import DeleteDriver from "../components/drivers/DeleteDriver";
 import EditDriver from "../components/drivers/EditDriver";
 import DriverService from "../services/DriverService";
 
+
 const HomePage = () => {
   interface IDriver {
     id: number;
@@ -36,7 +37,7 @@ const HomePage = () => {
         className="img-fluid mt-5 mx-auto d-block"
       />
       <div className="container">
-        <section className="row mt-3">
+        <section className="row mt-5">
           <h3 className="border-bottom border-dark p-4 mb-4 f1-black-font fs-1">
             F1 Event
           </h3>
@@ -81,3 +82,4 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
